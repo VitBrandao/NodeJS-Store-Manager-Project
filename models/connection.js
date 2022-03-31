cbe556e9e844aad77a97e5e235478878e18981a2
vitor <vitorsbsilva@gmail.com>
2022-03-31 15:00:27 -0300

    solved: mysql connection and permissions

--- models/connection.js
+++ models/connection.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql2/promise');
 
-// const connection = mysql.createPool({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '28183232',
-//   database: 'StoreManager',
-//   // port: 3306,
-// });
-
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  // port: process.env.PORT,
-}); 
+  host: 'localhost',
+  user: 'vitor',
+  password: '28183228',
+  // database: 'StoreManager',
+  // port: 3306,
+});
+
+// const connection = mysql.createPool({
+//   host: process.env.MYSQL_HOST,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE,
+//   // port: process.env.PORT,
+// }); 
 
 module.exports = connection;
